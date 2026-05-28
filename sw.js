3665975ec93cc09e095bd46bb5034f794909caed
AlexDev <alexdev@hotmail.com>
2026-05-28 12:40:07 -0600

    DEV9 -Se idento el archivo sw.js

--- sw.js
+++ sw.js
@@ -8,16 +8,19 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', e =>
+{
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', e =>
+{
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', e =>
+{
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
